01cb46f466d93166fb69c82ffb7a530cd55d9912
voravitl <voravitl@gmail.com>
2020-09-22 17:21:54 +0700

    Update gateway and route for ingress

--- scripts/get-urls.sh
+++ scripts/get-urls.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 export FRONTEND_URL=$(oc get route frontend -n $USERID -o jsonpath='{.spec.host}')
-export GATEWAY_URL=$(oc get route istio-ingressgateway -o jsonpath='{.spec.host}' -n $USERID-istio-system)
+export GATEWAY_URL=$(oc get route frontend -o jsonpath='{.spec.host}' -n $USERID-istio-system)
 export KIALI_URL=$(oc get route kiali -o jsonpath='{.spec.host}' -n $USERID-istio-system)
 export JAEGER_URL=$(oc get route jaeger -o jsonpath='{.spec.host}' -n $USERID-istio-system)
 echo "FRONTEND_URL=$(oc get route frontend -n $USERID -o jsonpath='{.spec.host}')"
